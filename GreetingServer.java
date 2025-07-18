ffe49c8971142bd98f2774b3b29f8e4e2181e229
Your Name <your.email@example.com>
2025-07-19 02:21:19 +0300

    update GreetingServer timeout and project configuration for JDK 21

--- GreetingServer.java
+++ GreetingServer.java
@@ -7,7 +7,7 @@ public class GreetingServer extends Thread {
 
     public GreetingServer(int port) throws IOException {
         serverSocket = new ServerSocket(port);
-        serverSocket.setSoTimeout(60000);
+        serverSocket.setSoTimeout(10000);
     }
 
     public void run() {
